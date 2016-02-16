9514ba81fae80cb5986b4f635d00cc18c307d668
Ian Dobson <Ian Dobson>
2016-02-15 19:36:13 -0700

    Test

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+var canvasHeight = "800";
+var canvasWidth = "800";
+var canvas = null;
+var context
+
+/*class Ball{
+  constructor(height, width, xPos, yPos){
+    this.height = height;
+    this.width = width;
+    this.xPos = xPos;
+    this.yPos = yPos;
+  }
+
+  function drawBall(){
+  }
+  function update(){
+  }
+}*/
+
+function startCanvas(){
+  canvas = document.getElementById("myCanvas");
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  context = canvas.getContext("2d");
+}
+
+function setUpCanvas(){
+
+}
+
+function AnimateCanvas(){
+
+}
+
+function ClearCanvas(){
+
+}
